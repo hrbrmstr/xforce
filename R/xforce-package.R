3d337b43d43a757e3642157b4384248c642c4586
boB Rudis <bob@rud.is>
2018-12-11 03:53:34 -0500

    R package repo initialization complete

---
+++ R/xforce-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/xforce>
+#' - BugReports: <https://gitlab.com/hrbrmstr/xforce/issues>
+#' 
+#' @md
+#' @name xforce
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
